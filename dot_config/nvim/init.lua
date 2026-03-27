e35df785665db9298a40616790a2c9ef9c533093
Ar52015 <aryan.arora2015@gmail.com>
2026-03-28 02:51:31 +0530

    nvim-config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Nerd Font installed
+vim.g.nerd_font_installed = true
+
+-- Adding line numbers to nvim
+vim.opt.number = true
+vim.opt.relativenumber = true
